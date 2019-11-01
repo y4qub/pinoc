be1fbabd013dbfbc68e81a3862833697c69b5608
unknown <vignedev@gmail.com>
2019-11-01 21:49:46 +0100

    some things i have done idk sayori best girl

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -15,24 +15,31 @@ export class BackendService {
       .pipe(map(this.filterOutComments))
   }
 
-  private filterOutComments = (events: Presentation[]) => events.filter(event => typeof event == 'object')
+  private filterOutComments = (events: Presentation[]) => events.filter(event => typeof event == 'object' && !event.disable)
 
   getRooms() {
 
     return this.http.get(this.endpoint)
       .pipe(
+        map(this.filterOutComments),
         map((events: Presentation[]) => {
 
-          let rooms: string[] = []
+          let rooms : object = {}
+          events.forEach(event => {
+            rooms[event.room] = null
+          })
+          return Object.keys(rooms)
+
+          /*let rooms: string[] = []
 
           events.forEach(event => {
 
-            if (rooms.findIndex(element => element == event.room) == -1)
+            if (event.room && rooms.findIndex(element => element == event.room) == -1)
               rooms.push(event.room)
 
           })
 
-          return rooms
+          return rooms*/
 
         })
       )
